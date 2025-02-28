0876213b9f33f1cd47e95ca1ec955d7d5cdc041a
JosePedroGB <josepedrogb2207@gmail.com>
2025-02-27 20:40:50 -0600

    ingresar correjido

--- src/components/registro.js
+++ src/components/registro.js
@@ -116,7 +116,7 @@
             onClick={() => router.push('/paginaIncio')}
             className="bg-green-500 text-white px-4 py-3 rounded hover:bg-green-600 transition w-full"
           >
-            Ingersar
+            Ingresar
           </button>
         </div>
       </div>
